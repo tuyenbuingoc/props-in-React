57c469ee003d9de1368cc8936397097116b48e0b
tuyenbuingoc <buingoctuyen08@gmail.com>
2019-04-16 16:45:38 +0700

    Add setState and new todoItem

--- src/App.js
+++ src/App.js
@@ -1,17 +1,89 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoList from './TodoList/TodoList';
+import downarrow from './Img/down-arrow.svg';
 
 class App extends Component {
   constructor() {
     super();
-    this.TodoItems = [{title: "Ăn sáng", isComplete: true}, {title: "Ăn trưa"}, {title: "Ăn tối"}];
+    this.state = {
+      newItem: '',
+      TodoItems: [{title: "Ăn sáng", isComplete: true}, 
+      {title: "Ăn trưa", isComplete: false},
+      {title: "Ăn tối", isComplete: false}]
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onKeyUp = this.onKeyUp.bind(this);
   }
+  onItemClicked(item) {
+    return (event) => {
+      const isComplete = item.isComplete;
+      const {todoItems} = this.state;
+      const index = todoItems.indexOf(item);
+      this.setState({
+        TodoItems: [
+          ...todoItems.slice(0, index),
+          {
+            ...item,
+            isComplete: !isComplete
+          },
+          ...todoItems.slice(index+1)
+        ]
+      });
+    }
+  }
+  onKeyUp(event) {
+    if(event.keyCode === 13) { //Enter key is pressed
+      let text = event.target.value;
+      if(!text) {
+        return;
+      }
+      text = text.trim();
+      if(!text) {
+        return;
+      }
+      this.setState({
+        newItem: '',
+        TodoItems: [
+          {
+            title: text,
+            isComplete: false
+          },
+          ...this.state.TodoItems
+        ]
+      });
+    }
+  } 
+
+  onChange(event) {
+    this.setState({
+      newItem: event.target.value
+    });
+  }
+
   render() {
+    const {TodoItems, newItem} = this.state;
     return (
       <div className="App">
+        <h1>todos</h1>
+        <div className="newItem">
+          <img src={downarrow} 
+          alt="downarrow" width={24} height={24} 
+          >
+          </img>
+          <input type="text" 
+          placeholder="What needs to be done?"
+          value={newItem}
+          onChange={this.onChange}
+          onKeyUp={this.onKeyUp}/>
+        </div>
+        
         {
-          this.TodoItems.map((item) => <TodoList item={item} />)
+          TodoItems.length && TodoItems.map((item, index) => 
+          <TodoList 
+          key={index} 
+          item={item}
+          onClick={this.onItemClicked(item)} />)
         }
       </div>
     );
--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import './TodoList.css'
+import classnames from 'classnames';
+import circle from '../Img/circle.svg';
+import success from '../Img/success.svg';
 
 class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {isComplete: this.props.isComplete};
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        this.setState(state => ({
+          isComplete: !this.state.isComplete
+        }))
+    }
+
     render() {
-        
-            const {item} = this.props;
-            let className = 'TodoList';
-            if(item.isComplete) {
-                className += ' TodoList-complete';
-            }
-        
+        const {item} = this.props;
+
         return (
-            <div className={className}>
+            <div className={classnames('TodoList', {
+                'TodoList-complete': this.state.isComplete
+            })}>
+                <img src={this.state.isComplete ? success : circle}
+                width={24} height={24} alt="Checkbox"
+                onClick={this.handleClick}></img>
                 <p>
                     {item.title}
                 </p>
